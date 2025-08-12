aae1b44035a464a5f14861e5540539e689f6468b
Ryan Turner <ryan@turnrye.com>
2025-08-12 09:29:44 -0500

    fixup! fixup! chore(stale): remove stale code that was related to the orignal author's deployment specifics

--- meshview/http.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-
-async def run_server(bind, path):
-    runner = web.AppRunner(app)
-    await runner.setup()
-    for host in bind:
-        site = web.TCPSite(runner, host, 80)
-    await site.start()
